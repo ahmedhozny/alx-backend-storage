75b5008b717b5e862c4d1032e6cba3f29f05038e
Ahmed Hosny <ahmhosny2002@gmail.com>
2024-05-14 02:07:28 +0300

    0x01. NoSQL advanced tasks

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Improve 12-log_stats.py by adding the top 10 of the most present IPs
+"""
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('localhost', 27017)
+    collection = client.logs.nginx
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    for method in ("GET", "POST", "PUT", "PATCH", "DELETE"):
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    with_status = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(with_status))
+
+    """"""
+    print("IPs:")
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for top_ip in top_ips:
+        print("\t{}: {}".format(top_ip["_id"], top_ip["count"]))
